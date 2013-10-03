2285ec8f243d56b6c38a4c293457c971753d710e
Greet Pauwels <github@greetpauwels.net>
2013-10-03 15:40:11 +0200

    Trial version issue solved.

--- Funky Film Android/Funky Film Android UI/MainActivity.cs
+++ Funky Film Android/Funky Film Android UI/MainActivity.cs
@@ -1,35 +1,31 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using Android.App;
 using Android.Content;
+using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
-using Android.OS;
+using Funky_Film.Android.UI;
 
-namespace Funky_Film.Android.UI
+namespace Funky_Film.Android.Ui
 {
+	[Activity (Label = "MainActivity", MainLauncher=true)]		
 
 	public class MainActivity : Activity
 	{
-	
-
 		protected override void OnCreate (Bundle bundle)
 		{
 			base.OnCreate (bundle);
-
-			// Set our view from the "main" layout resource
 			SetContentView (Resource.Layout.Main);
-
-			// Get our button from the layout resource,
-			// and attach an event to it
 			Button button = FindViewById<Button> (Resource.Id.myButton);
-			
+
 			button.Click += (sender, e) => {
 				StartActivity (typeof(SearchActivity));
 			};
-
 		}
 	}
 }
-
 
